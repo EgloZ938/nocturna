95d39ade6766ab30085f3123fc4324dc9f412684
EgloZ938 <theo.evon@epitech.eu>
2023-11-13 16:07:25 +0100

    ajout page register + opti images

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'register/new'
 
   root 'accueil#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
