ba840ead84ee63a5e176f9c95a01480b055f5485
EgloZ938 <theo.evon@epitech.eu>
2023-11-14 16:09:15 +0100

    detection navigateur + modif du son

--- app/assets/javascripts/accueil.js
+++ app/assets/javascripts/accueil.js
@@ -1,105 +1,116 @@
-    let music = new Audio('https://evarthel.com/mp3/medieval-fantasy-142837.mp3');
-    let effet = new Audio('https://evarthel.com/mp3/click-button-140881-%5bAudioTrimmer.com%5d(1).mp3');
+let music = new Audio('https://evarthel.com/mp3/medieval-fantasy-142837.mp3');
+let effet = new Audio('https://evarthel.com/mp3/click-button-140881-%5bAudioTrimmer.com%5d(1).mp3');
 
-    window.addEventListener("load", () => {
-        let param = "none";
-        audioMusique(param);
-        let storageMusique = localStorage.getItem('volume-musique');
-        let storageEffet = localStorage.getItem('volume-effet');
-        if(storageMusique == null){
-            localStorage.setItem("volume-musique","1");
-        }
-        if(storageEffet == null){
-            localStorage.setItem("volume-effet", "1");
-        }
-        volumeStorage();
-    });
+let storageMusique = localStorage.getItem('volume-musique');
+let storageEffet = localStorage.getItem('volume-effet');
 
-    let elem = document.getElementsByClassName("bg");
-    let length = elem.length;
-    for(let i = 0; i < length; i++){
-        elem[i].addEventListener("mouseenter", (e) =>{
-            mouseover(e);
-        })
+let userAgent = navigator.userAgent;
+let resultat = userAgent.includes("Firefox");
+
+if(resultat == true){
+    let status = true;
+    let param = "none";
+    audioMusique(param);
+}
+else{
+    let status = false;
+}
+
+window.addEventListener("load", () => {
+    if (storageMusique == null) {
+        localStorage.setItem("volume-musique", "1");
     }
-    
-    function mouseover(e){
-        audioEffet("none");
+    if (storageEffet == null) {
+        localStorage.setItem("volume-effet", "1");
     }
+    volumeStorage();
+});
 
-    function audioMusique(param){
-        if(param == "none"){
-            music.play();
-        }
-        else{
-            let value = param/10;
-            music.volume = value;
-        }
-        
-        music.addEventListener('ended', function() {
-            music.currentTime = 0;
-            music.play();
-        }, false);
+let elem = document.getElementsByClassName("bg");
+let length = elem.length;
+for (let i = 0; i < length; i++) {
+    elem[i].addEventListener("mouseenter", (e) => {
+        mouseover(e);
+    })
+}
 
+function mouseover(e) {
+    audioEffet("none");
+}
+
+function audioMusique(param) {
+    if (param == "none") {
+        music.play();
     }
-
-    function audioEffet(param){
-        if(param == "none"){
-            effet.play();
-        }
-        else{
-            let value = param/10;
-            effet.volume = value;
-        }
+    else {
+        let value = param / 10;
+        music.volume = value;
     }
 
-    function volumeStorage(){
-        let storageMusique = localStorage.getItem('volume-musique');
-        let storageEffet = localStorage.getItem('volume-effet');
+    music.addEventListener('ended', function () {
+        music.currentTime = 0;
+        music.play();
+    }, false);
 
-        music.volume = storageMusique;
-        effet.volume = storageEffet;
+}
 
-        document.getElementById("volume-music").value = storageMusique*10;
-        document.getElementById("volume-effect").value = storageEffet*10;
+function audioEffet(param) {
+    if (param == "none") {
+        effet.play();
     }
+    else {
+        let value = param / 10;
+        effet.volume = value;
+    }
+}
 
-    document.getElementById("option-button").addEventListener("click", () =>{
-        let elemOption = document.getElementById("options-menu");
-        let elemMenu = document.getElementById("main-menu");
-        elemOption.style.display = "block";
-        elemMenu.style.display = "none";
-    })
+function volumeStorage() {
+    let storageMusique = localStorage.getItem('volume-musique');
+    let storageEffet = localStorage.getItem('volume-effet');
 
-    document.getElementById("retour-option").addEventListener("click", () =>{
-        let elemOption = document.getElementById("options-menu");
-        let elemMenu = document.getElementById("main-menu");
-        elemOption.style.display = "none";
-        elemMenu.style.display = "block";
-    })
+    music.volume = storageMusique;
+    effet.volume = storageEffet;
 
-    document.getElementById("audio-button").addEventListener("click", () =>{
-        let elemAudio = document.getElementById("audio-menu");
-        let elemOption = document.getElementById("options-menu");
-        elemAudio.style.display = "block";
-        elemOption.style.display = "none";
-    })
+    document.getElementById("volume-music").value = storageMusique * 10;
+    document.getElementById("volume-effect").value = storageEffet * 10;
+}
 
-    document.getElementById("retour-audio").addEventListener("click", () =>{
-        let elemAudio = document.getElementById("audio-menu");
-        let elemOption = document.getElementById("options-menu");
-        elemAudio.style.display = "none";
-        elemOption.style.display = "block";
-    })
+document.getElementById("option-button").addEventListener("click", () => {
+    let elemOption = document.getElementById("options-menu");
+    let elemMenu = document.getElementById("main-menu");
+    elemOption.style.display = "block";
+    elemMenu.style.display = "none";
+})
 
-    document.getElementById("volume-music").addEventListener("input", (e) =>{
-        let param = e.target.value;
-        audioMusique(param);
-        localStorage.setItem("volume-musique", param/10);
-    })
+document.getElementById("retour-option").addEventListener("click", () => {
+    let elemOption = document.getElementById("options-menu");
+    let elemMenu = document.getElementById("main-menu");
+    elemOption.style.display = "none";
+    elemMenu.style.display = "block";
+})
 
-    document.getElementById("volume-effect").addEventListener("input", (e) =>{
-        let param = e.target.value;
-        audioEffet(param);
-        localStorage.setItem("volume-effet", param/10);
-    })
+document.getElementById("audio-button").addEventListener("click", () => {
+    let elemAudio = document.getElementById("audio-menu");
+    let elemOption = document.getElementById("options-menu");
+    elemAudio.style.display = "block";
+    elemOption.style.display = "none";
+})
+
+document.getElementById("retour-audio").addEventListener("click", () => {
+    let elemAudio = document.getElementById("audio-menu");
+    let elemOption = document.getElementById("options-menu");
+    elemAudio.style.display = "none";
+    elemOption.style.display = "block";
+})
+
+document.getElementById("volume-music").addEventListener("input", (e) => {
+    let param = e.target.value;
+    audioMusique(param);
+    localStorage.setItem("volume-musique", param / 10);
+})
+
+document.getElementById("volume-effect").addEventListener("input", (e) => {
+    let param = e.target.value;
+    audioEffet(param);
+    localStorage.setItem("volume-effet", param / 10);
+})
